2401e7e3fa3fd6d46933eac3a7ef7af7ca6197a7
James Liu <jamesliu1991@gmail.com>
2019-12-11 17:24:36 -0800

    added callback for barchart

--- app.R
+++ app.R
@@ -29,80 +29,47 @@ app <- Dash$new(external_stylesheets = "https://codepen.io/chriddyp/pen/bWLwgP.c
 yaxisKey <- tibble(label = c("GDP Per Capita", "Life Expectancy", "Population"),
                    value = c("gdpPercap", "lifeExp", "pop"))
 
-yaxisDropdown <- dccDropdown(
-  # TODO: Add id to component
-  # TODO: use yaxisKey (from above) and refactor the code below using
-  # purrr:map (tidyverse) instead of manually listing each option
-  # Reference: https://adv-r.hadley.nz/functionals.html
+bar_chart_radio_items <- dccRadioItems(
+  id = "bar_chart_radio_items",
   options=list(
-    list(label = "GDP Per Capita", value = "gdpPercap"),
-    list(label = "Life Expectancy", value = "lifeExp"),
-    list(label = "Population", value = "pop")
+    list(label = "first world", value = 1),
+    list(label = "all statuses", value = 0)
   ),
-  value = "gdpPercap"
+  value = 1
 )
 
-# TODO: Use a function make_graph() to create the graph.  
-# Note: The make_graph() function is created for you already.
-# Once you understand how it works, uncomment it out to use it
-# and then delete the static plot starting after this chunk.
-#
-############ START make_graph() function to use ############
-# Uses default parameters such as all_continents for initial graph
-# all_continents <- unique(gapminder$continent)
-# make_graph <- function(years=c(1952, 2007), 
-#                        continents=all_continents,
-#                        yaxis="gdpPercap"){
 
-#   # gets the label matching the column value
-#   y_label <- yaxisKey$label[yaxisKey$value==yaxis]
-
-#   #filter our data based on the year/continent selections
-#   data <- gapminder %>%
-#     filter(year >= years[1] & year <= years[2]) %>%
-#     filter(continent %in% continents)
-
-#   # make the plot!
-#   # on converting yaxis string to col reference (quosure) by `!!sym()`
-#   # see: https://github.com/r-lib/rlang/issues/116
-#   p <- ggplot(data, aes(x=year, y=!!sym(yaxis), colour=continent)) +
-#     geom_point(alpha=0.6) +
-#     scale_color_manual(name="Continent", values=continent_colors) +
-#     scale_x_continuous(breaks = unique(data$year))+
-#     xlab("Year") +
-#     ylab(y_label) +
-#     ggtitle(paste0("Change in ", y_label, " Over Time")) +
-#     theme_bw()
-
-#   ggplotly(p)
-# }
 ############ END make_graph() function to use ############
 
-# Graph (no change from app2)
-# TODO: once you create make_graph, remove this static ggplot plot below
-### START CODE TO REMOVE
-# p <- ggplot(gapminder, aes(x=year, y=gdpPercap, colour=continent)) +
-#   geom_point(alpha=0.6) +
-#   scale_color_manual(name="Continent", values=continent_colors) +
-#   xlab("Year") +
-#   ylab("GDP Per Capita") +
-#   theme_bw()
-### END CODE TO REMOVE
 
-
-return_fatality_barchart <- function(value = 0){
+return_fatality_barchart <- function(value = 1){
   
   chart_2_data <- read_csv("chart_2_data.csv")
+  chart_2_data$airline <- reorder(chart_2_data$airline,
+                                  -chart_2_data$total_fatalities_per_b_avail_seat_km)
   
-  fatality_barchart <- chart_2_data %>%
-    ggplot(aes(x = airline, 
-               y = total_fatalities_per_b_avail_seat_km)) +
-    geom_bar(stat = "identity",
-             fill = "blue") +
-    coord_flip() + 
-    labs(x = "Airlines",
-         y = "adjusted rate of fatalities",
-         title = "Number of fatalities for airlines that had an incident between 1985 and 2014")
+  
+  if(value != 0) {
+    fatality_barchart <-ggplot(data = chart_2_data,
+                               aes(x = airline, 
+                                   y = total_fatalities_per_b_avail_seat_km,
+                                   fill = first_world))+
+      geom_bar(stat = "identity") +
+      coord_flip() + 
+      labs(x = "Airlines",
+           y = "adjusted rate of fatalities",
+           title = "Number of fatalities for airlines that had an incident between 1985 and 2014") 
+  }else{
+    fatality_barchart <-ggplot(data = chart_2_data,
+                                 aes(x = airline, 
+                                     y = total_fatalities_per_b_avail_seat_km))+
+      geom_bar(stat = "identity",
+               fill = "blue") +
+      coord_flip() + 
+      labs(x = "Airlines",
+           y = "adjusted rate of fatalities",
+           title = "Number of fatalities for airlines that had an incident between 1985 and 2014") 
+  }
   fatality_barchart
   }
 
@@ -149,7 +116,7 @@ app$layout(
       # htmlLabel('Select continents:'),
       # continentDropdown,
       # htmlLabel('Select y-axis metric:'),
-      yaxisDropdown,
+      bar_chart_radio_items,
       #end selection components
       horizontal_bar_chart,
       jitter_var_fatality_chart,
@@ -162,19 +129,17 @@ app$layout(
 ### # TODO: Add callbacks to enable interactivity
 
 # # Adding callbacks for interactivity
-# app$callback(
+app$callback(
 #   # update figure of gap-graph
-#   output=list(id = 'gap-graph', property='figure'),
+  output=list(id = 'fatality_barchart', property='figure'),
 #   
 #   # based on values of year, continent, y-axis components
 #   # TODO: Update the IDs of the components (note: remember that order matters!!)
-#   params=list(input(id = 'CORRECT_ID_HERE', property='value'),
-#               input(id = 'CORRECT_ID_HERE', property='value'),
-#               input(id = 'CORRECT_ID_HERE', property='value')),
+  params=list(input(id = 'bar_chart_radio_items', property='value')),
 #
 #   # this translates your list of params into function arguments
-#   function(year_value, continent_value, yaxis_value) {
-#     make_graph(year_value, continent_value, yaxis_value)
-#   })
+  function(radio_value) {
+    ggplotly(return_fatality_barchart(radio_value))
+  })
 
 app$run_server()
